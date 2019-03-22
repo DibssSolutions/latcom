bbcd31ea704f889dfa043e31a305dd3fdb050b34
Валерий <kaktuz34@gmail.com>
2019-03-22 13:33:50 +0200

    update scrollbar

--- src/js/components/_mainScrollBar.js
+++
@@ -1,6 +0,0 @@
-import PerfectScrollbar from 'perfect-scrollbar';
-var ps = new PerfectScrollbar('.main', {
-  wheelSpeed: 2,
-  wheelPropagation: true,
-  minScrollbarLength: 20
-});
--- src/js/components/index.js
+++ src/js/components/index.js
@@ -2,7 +2,7 @@ import './_modal';
 import './_select';
 import './_initScrollbar';
 import './_slider';
-import './_mainScrollBar';
+import './_scrollBar';
 import './_accordion';
 import './_toggleTableBtn';
 import './_filters';
