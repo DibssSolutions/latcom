cba741dc65b330b7764b1371081c265554b928a8
Валерий <kaktuz34@gmail.com>
2019-03-22 13:28:12 +0200

    added scroll

---
+++ src/js/components/_mainScrollBar.js
@@ -0,0 +1,6 @@
+import PerfectScrollbar from 'perfect-scrollbar';
+var ps = new PerfectScrollbar('.main', {
+  wheelSpeed: 2,
+  wheelPropagation: true,
+  minScrollbarLength: 20
+});
--- src/js/components/index.js
+++ src/js/components/index.js
@@ -2,7 +2,7 @@ import './_modal';
 import './_select';
 import './_initScrollbar';
 import './_slider';
-// import './_initFancybox';
+import './_mainScrollBar';
 import './_accordion';
 import './_toggleTableBtn';
 import './_filters';
