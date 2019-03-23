8640d0b7eb35914f6b2166500ded0332773bb200
Валерий <kaktuz34@gmail.com>
2019-03-23 17:19:26 +0200

    upd

--- src/js/components/_scrollBar.js
+++ src/js/components/_scrollBar.js
@@ -1,2 +1,10 @@
 import PerfectScrollbar from 'perfect-scrollbar';
 var ps = new PerfectScrollbar('.js-scroll-bar');
+
+const containers = document.querySelectorAll('.js-scroll-bar');
+if (containers) {
+  for (var i = 0; i <= containers.length - 1; i++) {
+    new PerfectScrollbar(containers[i]);
+  }
+}
+
