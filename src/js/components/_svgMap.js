7035964745dc5e002301dd04514cc926461e607b
Валерий <kaktuz34@gmail.com>
2019-03-25 17:51:04 +0200

    svg map desctop done

--- src/js/components/_svgMap.js
+++ src/js/components/_svgMap.js
@@ -1,28 +1,40 @@
 import mapael from 'jquery-mapael';
 import '../../../node_modules/jquery-mapael/js/maps/world_countries.js';
+import {LOAD_DATA} from '../utils.js';
 
-$('.js-svg-map').mapael({
-  map : {
-    name : 'world_countries',
-    defaultArea: {
-      attrs: {
-        fill: '#f2f2f2',
-        stroke: '#bababa',
-        'stroke-width': 0.2,
-        'stroke-linejoin': 'round'
-      },
-      attrsHover: {
-        fill: '#1c7efb'
-      }
-    },
-    zoom: {
-      enabled: true,
-      maxLevel: 17,
-      init: {
-        latitude: -30.717079,
-        longitude: -46.00116,
-        level: 10
-      }
+const maps = $('.js-svg-map');
+maps.each((i,el) => {
+  const thisPath = $(el).data('json');
+  LOAD_DATA({
+    path: thisPath,
+    callback: data => {
+      $(el).mapael({
+        map : {
+          name : 'world_countries',
+          defaultArea: {
+            attrs: {
+              fill: '#f2f2f2',
+              stroke: '#bababa',
+              'stroke-width': 0.2,
+              'stroke-linejoin': 'round'
+            },
+            attrsHover: {
+              fill: '#1c7efb'
+            }
+          },
+          zoom: {
+            enabled: true,
+            maxLevel: 17,
+            init: {
+              latitude: -30.717079,
+              longitude: -46.00116,
+              level: 10
+            }
+          }
+        },
+        areas: data[0]
+      });
     }
-  }
+  });
 });
+
