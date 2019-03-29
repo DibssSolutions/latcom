4636fc91e8a143f3663df6a53a9b8f124c00dc62
Валерий <kaktuz34@gmail.com>
2019-03-29 14:50:09 +0200

    update maps

--- src/js/components/_accordion.js
+++ src/js/components/_accordion.js
@@ -1,34 +1,3 @@
-// import { OPEN, BODY } from '../constants';
-// const accordeons = $('.js-accordion');
-// const accordeonsWrap = $('.js-accordion-wrap');
-
-// BODY.on('click', e => {
-//   const parent = $(e.target).closest('.js-accordion-trigger')[0];
-//   if (!parent) return;
-
-//   const that = $(e.target).closest('.js-accordion');
-//   const thatAccordeonWrap = that.find('.js-accordion-wrap');
-
-//   e.preventDefault();
-//   if (!that.hasClass(OPEN)) {
-//     accordeons.removeClass(OPEN);
-//     accordeonsWrap.slideUp(350);
-//     that.addClass(OPEN);
-//     thatAccordeonWrap.slideDown(350);
-//   } else {
-//     that.removeClass(OPEN);
-//     thatAccordeonWrap.slideUp(350);
-//   }
-// });
-
-// accordeons.each(function() {
-//   const _this = $(this);
-//   const _accordeonWrap = _this.find('.js-accordion-wrap');
-//   if (_this.hasClass(OPEN)) {
-//     _accordeonWrap.show();
-//   }
-// });
-
 import { OPEN, WIN, WIN_WIDTH } from '../constants';
 
 class ACCORDION {
@@ -60,7 +29,6 @@ class ACCORDION {
       };
       const openItemInit = () => {
         const WIN_WIDTH = WIN.width();
-        console.log(initWidth);
         if (!openFlug && !initWidth) return;
         if (openFlug && !initWidth) {
           openItem();
